7286b991769f9048fab801bade31db19cef16c87
Martin-Lndbl <martin@lndbl.de>
2025-04-01 21:49:29 +0200

    setup mailserver and move nginx to host

--- containers/mailserver/default.nix
+++ containers/mailserver/default.nix
@@ -1,41 +1,45 @@
-{ ... }:
+{ inputs, ... }:
 {
   containers.mailserver = {
     autoStart = true;
     privateNetwork = true;
+    hostAddress = "10.0.0.20";
+    localAddress = "10.0.0.21";
     forwardPorts = [
-      {
-        hostPort = 80;
-        containerPort = 80;
-      }
-      {
-        hostPort = 443;
-        containerPort = 443;
-      }
+      { hostPort = 25; }
+      { hostPort = 143; }
+      { hostPort = 465; }
+      { hostPort = 587; }
+      { hostPort = 993; }
     ];
+    bindMounts."/var/lib/acme".isReadOnly = true;
+
     config =
       { ... }:
       {
+        imports = [ inputs.mailserver.nixosModule ];
         mailserver = {
           enable = true;
           fqdn = "mail.lndbl.de";
           domains = [ "lndbl.de" ];
 
-          # A list of all login accounts. To create the password hashes, use
           # nix-shell -p mkpasswd --run 'mkpasswd -sm bcrypt'
           loginAccounts = {
-            "user1@example.com" = {
-              hashedPasswordFile = "/a/file/containing/a/hashed/password";
-              aliases = [ "postmaster@example.com" ];
+            "martin@lndbl.de" = {
+              hashedPassword = "$2b$05$yQxecWYH.GJ2m0W2QBharOU4qhiTEJyWI.LGnc83CpINdMOExcGMG";
+              aliases = [ ];
+              catchAll = [ "lndbl.de" ];
             };
           };
 
-          # Use Let's Encrypt certificates. Note that this needs to set up a stripped
-          # down nginx and opens port 80.
-          certificateScheme = "acme-nginx";
+          certificateScheme = "acme";
         };
+
         security.acme.acceptTerms = true;
-        security.acme.defaults.email = "security@lndbl.de";
+        security.acme.defaults.email = "acme@lndbl.de";
+        security.acme.defaults.webroot = "/var/lib/acme/.challenges";
+
+        system.stateVersion = "25.05";
       };
   };
 }
--- containers/nginx/default.nix
+++ containers/nginx/default.nix
@@ -1,80 +1,52 @@
 { ... }:
 {
-  containers.nginx = {
-    autoStart = true;
-    privateNetwork = true;
-    hostAddress = "10.0.0.10";
-    localAddress = "10.0.0.11";
-    forwardPorts = [
-      {
-        hostPort = 80;
-        containerPort = 80;
-      }
-      {
-        hostPort = 443;
-        containerPort = 443;
-      }
-    ];
-    bindMounts."/var/lib/acme".isReadOnly = false;
 
-    config =
-      {
-        lib,
-        ...
-      }:
-      {
-        security.acme.acceptTerms = true;
-        security.acme.defaults.email = "admin+acme@lndbl.de";
+  security.acme.acceptTerms = true;
 
-        users.users.nginx.extraGroups = [ "acme" ];
+  users.users.nginx.extraGroups = [ "acme" ];
 
-        services.nginx = {
-          enable = true;
-          virtualHosts = {
-            "acmechallenge.lndbl.de" = {
-              serverAliases = [ "*.lndbl.de" ];
-              locations."/.well-known/acme-challenge" = {
-                root = "/var/lib/acme/.challenges";
-              };
-              locations."/" = {
-                return = "301 https://$host$request_uri";
-              };
-            };
-            "foo.lndbl.de" = {
-              forceSSL = true;
-              useACMEHost = "foo.lndbl.de";
-              locations."/".index = "index.html";
-            };
-          };
+  services.nginx = {
+    enable = true;
+    virtualHosts = {
+      "acmechallenge.lndbl.de" = {
+        serverAliases = [ "*.lndbl.de" ];
+        locations."/.well-known/acme-challenge" = {
+          root = "/var/lib/acme/.challenges";
         };
-
-        security.acme.certs = {
-          "foo.lndbl.de" = {
-            webroot = "/var/lib/acme/.challenges";
-            email = "foo@lndbl.de";
-            group = "nginx";
-            # extraDomainNames = [ "mail.lndbl.de" ];
-          };
-          "mail.lndbl.de" = {
-            webroot = "/var/lib/acme/.challenges";
-            email = "mail@lndbl.de";
-            group = "nginx";
-          };
+        locations."/" = {
+          return = "301 https://$host$request_uri";
         };
-
-        services.resolved.enable = true;
-        networking = {
-          firewall = {
-            enable = true;
-            allowedTCPPorts = [
-              80
-              443
-            ];
-          };
-          useHostResolvConf = lib.mkForce false;
-        };
-
-        system.stateVersion = "25.05";
       };
+      "foo.lndbl.de" = {
+        forceSSL = true;
+        useACMEHost = "foo.lndbl.de";
+        locations."/".index = "index.html";
+      };
+    };
+  };
+
+  security.acme.certs = {
+    "foo.lndbl.de" = {
+      webroot = "/var/lib/acme/.challenges";
+      email = "foo@lndbl.de";
+      group = "nginx";
+      # extraDomainNames = [ "mail.lndbl.de" ];
+    };
+    "mail.lndbl.de" = {
+      webroot = "/var/lib/acme/.challenges";
+      email = "mail@lndbl.de";
+      group = "nginx";
+      reloadServices = [ "container@mailserver.service" ];
+    };
+  };
+
+  networking = {
+    firewall = {
+      enable = true;
+      allowedTCPPorts = [
+        80
+        443
+      ];
+    };
   };
 }
